37db3c3184417febf6b8d44c62ba91f5763d9574
Evan Polekoff <eapolekoff@wpi.edu>
2015-01-24 17:04:58 -0500

    Player move action is set. I'm going to start debugging to try to get this to visually work.

---
+++ Assets/Scripts/ActionManagement/ActionTypes/MoveAction.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAction : AbsAction, IAction
+{
+
+
+    public MoveAction(EActionDirection direction)
+        : base(direction)
+    {
+    }
+
+    public EActionType GetActionType()
+    {
+        return EActionType.Move;
+    }
+
+    public void ReSolve(ReActionStatus reaction, ActionStatus status)
+    {
+        Player attacker = status.OwnerPlayer.GetPlayerPiece();
+        Player enemy = reaction.OwnerPlayer.GetPlayerPiece();
+
+        //Start by allowing the enemy to move
+        EActionDirection enemyDirection = reaction.Direction;
+        EnemyMoveReaction(enemy.GetPosition(), GetTargetPosition(enemy.GetPosition(), enemyDirection));
+
+        //Perform calculations to assess the action.
+        Vector2 targetPosition = GetTargetPosition(attacker.GetPosition(), direction);
+        bool enemyInFront = (targetPosition == enemy.GetPosition());  //The enemy is in the way
+        EReActionType enemyReaction = reaction.ReactionType.GetReactionType();
+
+        //If you move into a spot dodge, swap the player positions
+        if (enemyInFront && enemyReaction == EReActionType.Spot)
+        {
+            //Swap positions
+            GameController.Instance.GetPieceStructure().SwapPiecePositions((int)attacker.GetPosition().x, (int)attacker.GetPosition().y,
+                                                                    (int)targetPosition.x, (int)targetPosition.y);
+        }
+        //If nothing else is blocking the attacker, move him.
+        else
+        {
+            if (GameController.Instance.GetPieceStructure().isSpaceMovable((int)targetPosition.x, (int)targetPosition.y))
+            {
+                //Move the player
+                GameController.Instance.GetPieceStructure().MovePiece((int)attacker.GetPosition().x, (int)attacker.GetPosition().y,
+                                                                        (int)targetPosition.x, (int)targetPosition.y);
+            }
+        }
+    }
+}
